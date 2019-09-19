839e0cec936e70c3e47fc5a18b6d4ccec2469147
Srikalyan Megati <kalmegati@gmail.com>
2019-09-19 15:13:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,8 +24,8 @@ def monopoly_with_third_tier
 	  :names => {
 	    :reading_railroad => {}, 
 	    :pennsylvania_railroad => {}, 
-	    :shortline_railroad => {},
-	    :
+	    :shortline_railroad => {}
+	    
 	  },
 	  :rent_in_dollars => {
 	    :one_piece_owned => 25, 
